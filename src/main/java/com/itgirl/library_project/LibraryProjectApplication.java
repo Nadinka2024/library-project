d88e52234ec2f7ced0d08e49e507f7f3b8744610
nadezhdachurilo <nadezhdachurilo@gmail.com>
2025-01-14 16:58:49 +0100

    new8

--- src/main/java/com/itgirl/library_project/LibraryProjectApplication.java
+++ src/main/java/com/itgirl/library_project/LibraryProjectApplication.java
@@ -2,11 +2,13 @@ package com.itgirl.library_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.itgirl.library_project.entity")
 public class LibraryProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryProjectApplication.class, args);
 	}
 }
+
