525bfb93ac2e92e21687c6c3a008be873e36749e
nadezhdachurilo <nadezhdachurilo@gmail.com>
2025-01-16 16:47:35 +0100

    new12

--- src/test/java/com/itgirl/library_project/TestConfig.java
+++ src/test/java/com/itgirl/library_project/TestConfig.java
@@ -1,16 +1,13 @@
 package com.itgirl.library_project;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
-
 class LibraryProjectApplicationTests {
-	@Test
-    void contextLoads() {
-	}
 
+	@Test
+	void contextLoads() {
+	}
 
 }
